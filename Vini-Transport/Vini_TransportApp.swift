d29e4c0a8e7f460cdf4a4f5e01c64f38e3c9c097
yanncombette <ycombette.dev@gmail.com>
2024-04-05 14:00:10 +0200

    Initial Commit

---
+++ Vini-Transport/Vini_TransportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vini_TransportApp.swift
+//  Vini-Transport
+//
+//  Created by yann combette on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Vini_TransportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
